54ef7cdd186da060eab171cf64b55ecb3125a38b
ashiqali.sayyad <ashiqali.sayyad@globallogic.com>
2020-06-13 21:12:05 +0530

    added actual logic to capture form factors using databinding

--- app/src/main/java/com/ashiq/sayyad/deviceformfactor/MainActivity.kt
+++ app/src/main/java/com/ashiq/sayyad/deviceformfactor/MainActivity.kt
@@ -2,11 +2,14 @@ package com.ashiq.sayyad.deviceformfactor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ashiq.sayyad.deviceformfactor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
     }
 }
